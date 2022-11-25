4ab6a3040dee97d047f51d012f761f5c8a6ea06c
Jilvo <jmillot41@gmail.com>
2022-11-25 14:54:32 +0100

    few changes && rename input && fusion_script

--- fichiers_et_constantes.py
+++ fichiers_et_constantes.py
@@ -2,13 +2,11 @@
 PARSER_FILTER_FILE = "input_datas\parse_filter.txt"
 METADONNES_MR_TRAM = "input_datas\metadonnes_TRAM.csv"
 LISTES_ARRETS_LIGNES = "input_datas/listes_arrets_lignes.csv"
-EXTRACTION_DARFEUILLE_MODIFIE = "input_datas/20221010_ExtratTCLDoc complete modifié.csv"
+EXTRACTION_DARFEUILLE_MODIFIE = "input_datas/ExtratTCLDoc.csv"
 EXTRACTION_DARFEUILLE_JUSTE_AA_ET_NON_SERBER = (
     "input_datas/Extraction Serber juste AA et non serber.csv"
 )
-EXTRACTION_DARFEUILLE_SANS_AR = (
-    "input_datas/20221010_ExtratTCLDoc complete modifié sans AR.csv"
-)
+EXTRACTION_DARFEUILLE_SANS_AR = "input_datas/ExtratTCLDoc complete modifié sans AR.csv"
 ### OUTPUTS MRTRAM ###
 DIRNAME_MRTRAM = f"""F:\BE PERSO (M)\Millot J\Oct 2015 Fin de garantie"""
 NOM_FICHIER_SORTIE_MR_TRAM = "output_datas/mr_tram_t6.csv"
@@ -19,7 +17,5 @@ ARBO_PDF = "output_datas/arborescence_semaly_pdf.csv"
 ARBO_TIF = "output_datas/arborescence_semaly_tif.csv"
 
 ### OUTPUTS AUTRE SERVEUR: ###
-FICHIER_ECHECS_A_COPIER_SUR_AUTRE_SERVEUR = (
-    "output_datas\erreurs restantes apres comparaison complete darfeuille.csv"
-)
+FICHIER_ECHECS_A_COPIER_SUR_AUTRE_SERVEUR = "fusion_finale\fusion_des_echecs.csv"
 DESTINATION_FICHIERS_COPIE = "output_datas/tcl_copy_files"
--- fusion_fichiers_csv.py
+++ fusion_fichiers_csv.py
@@ -1,21 +1,43 @@
 import pandas as pd
+import os
 
 # merging two csv files
-df = pd.concat(
-    map(
-        pd.read_csv,
-        [
-            "output_datas\listes des succes Tram Ligne T5 complet.csv",
-            "output_datas\listes des succes Liaison b_d complet.csv",
-        ],
-    ),
-    ignore_index=True,
-)
-print(df)
+def fusion():
+    print(" Ce script va vous permettre de fusionner des fichiers CSV ")
+    print("Bien faire attention au chemin du fichier, car sinon il faudra recommencer")
+    list_files = []
+    flag = False
+    while not flag:
+        print(
+            "Voulez vous ajouter un fichier CSV sous format 'nom_du_fichier', le script rajoutera l'extension et le sous dossier output_datas"
+        )
+        input_choice = input("O / N : ")
+        if input_choice.upper() == "O":
+            path_file = input("Entrez le nom du fichier : ")
+            path_file = "fusion_finale\\" + path_file + ".csv"
+            path = os.path.join(path_file)
+            list_files.append(path)
+            print("path_file = ", path)
+        elif input_choice.upper() == "N":
+            flag = True
+        else:
+            print("Je ne comprend pas votre réponse, réessayez ")
 
-df.to_csv(
-    "output_datas/fusion.csv",
-    sep=";",
-    index=False,
-    encoding="cp1252",
-)
+    df = pd.concat(
+        map(
+            pd.read_csv,
+            list_files,
+        ),
+        ignore_index=True,
+    )
+    print(df)
+
+    df.to_csv(
+        "output_datas/fusion.csv",
+        sep=";",
+        index=False,
+        encoding="cp1252",
+    )
+
+
+fusion()
